524f6894ab82f9be02ecc1646dc18f3ce4165ce5
Jaymin West <jayminwest@gmail.com>
2025-03-10 14:34:28 -0700

    refactor: Fix import path for DatabaseService in serverDatabaseService

--- app/services/database/serverDatabaseService.ts
+++ app/services/database/serverDatabaseService.ts
@@ -1,7 +1,7 @@
 import { createServerComponentClient } from '@supabase/auth-helpers-nextjs';
 import { cookies } from 'next/headers';
 import { Database } from '@/app/types/database';
-import { DatabaseService } from './databaseService';
+import { DatabaseService } from './DatabaseService';
 
 /**
  * Server-side extension of DatabaseService
